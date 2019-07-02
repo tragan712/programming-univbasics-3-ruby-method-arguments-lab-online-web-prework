2d4b42677708a6fca2fd702df5c33547f5daa387
Timothy Ragan <timothyragan7@gmail.com>
2019-07-02 17:32:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -23,9 +23,12 @@ end
 
 def introduction_with_language_optional(name)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+name="Edwin"
 language="Ruby"
 end
 
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+name="Edwin"
+language="Python"
 end
