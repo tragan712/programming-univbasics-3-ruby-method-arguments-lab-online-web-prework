0588f63a9ae169f52802946ab4a4eedac583aa34
Timothy Ragan <timothyragan7@gmail.com>
2019-07-02 17:37:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,29 +1,17 @@
 
 def introduction(name)  
   puts "Hi, my name is #{name}."
-name="Josh"
 end
 
-def introduction(name)  
-  puts "Hi, my name is #{name}."
-name="Dan"
-end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-name"Dan"
-language="Ember.js"
-end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-name="Josh"
-language="React"
 end
 
 def introduction_with_language_optional(name, language)
-  
-  puts "Hi, my name is #{name} and I am learning to program in #{+language}."
-language="Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+
 end
 
+introduction()
